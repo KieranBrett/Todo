a445b382d0c549a40e9d46ba689c27dbeb8e1b40
KieranBrett <brett.kieran@hotmail.com>
2021-06-29 14:36:47 +1200

    todo now deployed to makeatodo.web.app

--- src/nav/navBar.js
+++ src/nav/navBar.js
@@ -12,7 +12,7 @@ class NavBar extends React.Component {
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
-        <Link to="/" class="navbar-brand">Advanced App Dev</Link>
+        <Link to="/" class="navbar-brand">To Do App!</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -22,7 +22,7 @@ class NavBar extends React.Component {
               <Link to="/" class="nav-link">Home</Link>
             </li>
             <li class="nav-item active">
-              <Link to="/todo" class="nav-link">ToDo</Link>
+              <Link to="/todo" class="nav-link">My Lists</Link>
             </li>
             {/* <li class="nav-item active">
               <Link to="/groups" class="nav-link">Groups</Link>
