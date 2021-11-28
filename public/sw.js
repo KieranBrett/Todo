114302829cc4518fa5e1ed37e5df0de8edf16438
Kieran Brett <brett.kieran@hotmail.com>
2021-11-28 18:00:51 +1300

    Fixed fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -20,12 +20,40 @@ self.addEventListener('activate', (e) => {
     console.log('Service worker activated');
 });
 
-self.addEventListener('fetch', (e) => {
-    // console.log(e.request.url); // Logs all URLS
+// self.addEventListener('fetch', (e) => {
+//     // console.log(e.request.url); // Logs all URLS
 
-    e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
+//     e.respondWith(
+//         caches.match(e.request).then((res) => {
+//             return res || fetch(e.request);
+//         })
+//     )
+// });
+
+
+// fetch event listener for all requests
+self.addEventListener('fetch', function(event) {
+    // console.log('fetch event', event);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // caches.match() always resolves but in case of success response will have value
+            if (response !== undefined) {
+                return response;
+            } else {
+                return fetch(event.request).then(function(response) {
+                    // response may be used only once
+                    // we need to save clone to put one copy in cache and serve second one
+                    let responseClone = response.clone();
+
+                    caches.open('todolist').then(function(cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
+                    
+                }).catch(function() {
+                    return caches.match('/index.html');
+                });
+            }
         })
-    )
+    );
 });
